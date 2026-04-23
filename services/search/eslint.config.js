2905f8907cabc1982d6160afbb991aedadbdf5f3
Ben Farz <benfarz.bf25@gmail.com>
2026-04-22 19:04:15 -0600

    Init core for the Search Microservice (Dockerfile - ElasticSearch - package.json)

---
+++ services/search/eslint.config.js
@@ -0,0 +1,12 @@
+module.exports = [
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "script"
+    },
+    rules: {
+      "no-console": "off"
+    }
+  }
+];
